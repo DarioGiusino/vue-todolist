28e6bd43f98a8d9e702f0605585e549bed2eb31e
DarioGiusino <dario.giusino@gmail.com>
2023-01-11 16:05:22 +0100

    created tasks data and static list

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
     name: 'To Do List',
     data() {
         return {
-
+            tasks: [
+                {
+                    text: 'fare la spesa',
+                    done: false
+                },
+                {
+                    text: 'giocare alla play',
+                    done: true
+                }
+            ]
         }
     }
 });
