027b8a80d1cfaf693e6a59d8f9a09753774d08d1
DarioGiusino <dario.giusino@gmail.com>
2023-01-11 17:20:39 +0100

    add task (test)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ const app = Vue.createApp({
     name: 'To Do List',
     data() {
         return {
+            newTask: {
+                 text: '',
+                 done: false
+            },
             tasks: [
                 {
                     text: 'fare la spesa',
@@ -12,14 +16,17 @@ const app = Vue.createApp({
                 {
                     text: 'giocare alla play',
                     done: true
-                }
+                },
             ]
         }
     },
     methods: {
+        addTask(){
+            this.tasks.push(this.newTask);
+        },
         deleteTask(target){
             this.tasks.splice(target, 1);
-        }
+        },
     }
 });
 
